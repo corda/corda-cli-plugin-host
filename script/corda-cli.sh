d5cbc60d44ccd137fbd0e7acbfba3634e667b21b
Christian Sailer <christian.sailer@r3.com>
2022-05-10 16:06:16 +0100

    Fix startup scripts

--- script/corda-cli.sh
+++ script/corda-cli.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # get path to script
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPTPATH=$(dirname "$0")
 
 rootDir="$SCRIPTPATH/.."
 binDir="$rootDir/app/build/libs"
 pluginsDir="$rootDir/build/plugins"
 
-java  -Dpf4j.pluginsDir="$pluginsDir" -jar "$binDir/corda-cli-0.0.1-beta.jar" "$@"
+java  -Dpf4j.pluginsDir="$pluginsDir" -jar "$binDir/corda-cli-0.0.1-SNAPSHOT.jar" "$@"
