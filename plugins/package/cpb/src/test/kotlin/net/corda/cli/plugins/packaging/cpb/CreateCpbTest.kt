b23d9b13a0096e6db7073720330b38a4583ea0d3
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-07-12 18:13:31 +0100

    Sign the temp Cpb to produce the final outcome of this command which is a signed Cpb

--- plugins/package/cpb/src/test/kotlin/net/corda/cli/plugins/packaging/cpb/CreateCpbTest.kt
+++ plugins/package/cpb/src/test/kotlin/net/corda/cli/plugins/packaging/cpb/CreateCpbTest.kt
@@ -1,8 +1,13 @@
 package net.corda.cli.plugins.packaging.cpb
 
+import java.io.FileInputStream
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
+import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.cert.CertPath
+import java.security.cert.CertificateFactory
 import java.util.UUID
 import java.util.jar.Attributes
 import java.util.jar.JarEntry
@@ -23,13 +28,14 @@ class CreateCpbTest {
     val createCpb = CreateCpb()
 
     companion object {
+
         private fun checkCpbContainsEntries(cpb: Path, expectedEntries: List<String>) {
             JarInputStream(Files.newInputStream(cpb)).use {
                 assertTrue(it.manifest.mainAttributes.isNotEmpty())
                 assertTrue(it.manifest.mainAttributes[Attributes.Name("Corda-CPB-Format")] == "2.0")
                 assertTrue(it.manifest.mainAttributes[Attributes.Name("Corda-CPB-Upgrade")] == false.toString())
-                val jarEntries = mutableListOf<ZipEntry>()
 
+                val jarEntries = mutableListOf<ZipEntry>()
                 var jarEntry: JarEntry? = it.nextJarEntry
                 while (jarEntry != null) {
                     jarEntries.add(jarEntry)
@@ -40,6 +46,9 @@ class CreateCpbTest {
                 )
             }
         }
+
+        private val testKeyStore = Path.of(this::class.java.getResource("/signingkeys.pfx")?.toURI()
+            ?: error("signingkeys.pfx not found"))
     }
 
     private fun buildTestCpk(jars: List<String>): Path {
@@ -56,7 +65,7 @@ class CreateCpbTest {
     }
 
     @Test
-    fun `packCpksToUnsignedCpb packs CPKs into CPB`() {
+    fun `packCpksToUnsignedArchive packs CPKs into CPB`() {
         val cpk0 = buildTestCpk(
             listOf(
                 "lib/cpk0-lib.jar",
@@ -70,7 +79,7 @@ class CreateCpbTest {
             )
         )
 
-        val cpb = createCpb.packCpksToUnsignedCpb(sequenceOf(cpk0.toString(), cpk1.toString()), false)
+        val cpb = createCpb.packCpksToUnsignedArchive(sequenceOf(cpk0.toString(), cpk1.toString()), false)
 
         checkCpbContainsEntries(
             cpb,
@@ -82,7 +91,7 @@ class CreateCpbTest {
     }
 
     @Test
-    fun `packCpksToUnsignedCpb throws if CPK is missing`() {
+    fun `packCpksToUnsignedArchive throws if CPK is missing`() {
         val cpk0 = buildTestCpk(
             listOf(
                 "lib/cpk0-lib.jar",
@@ -92,14 +101,54 @@ class CreateCpbTest {
         val missingCpk = Path.of("missing.cpk")
 
         assertThrows(IllegalArgumentException::class.java) {
-            createCpb.packCpksToUnsignedCpb(sequenceOf(cpk0.toString(), missingCpk.toString()), false)
+            createCpb.packCpksToUnsignedArchive(sequenceOf(cpk0.toString(), missingCpk.toString()), false)
         }
     }
 
-
     @Test
-    fun `adds META-INF folder`() {
+    fun `on signing Cpb signature is added`() {
+        val cpk0 = buildTestCpk(
+            listOf(
+                "lib/cpk0-lib.jar",
+                "main-bundle0.jar"
+            )
+        )
+        val cpk1 =  buildTestCpk(
+            listOf(
+                "lib/cpk1-lib.jar",
+                "main-bundle1.jar"
+            )
+        )
 
+        val unisgnedCpb = createCpb.packCpksToUnsignedArchive(sequenceOf(cpk0.toString(), cpk1.toString()), false)
+        val signedCpbFile = Path.of(tempDir.toString(), "signed-cpb-${UUID.randomUUID()}.cpb")
+
+        val (privateKey, certPath) = getPrivateKeyAndCertPath()
+        createCpb.signJar(unisgnedCpb, signedCpbFile, privateKey, certPath, null)
+        JarInputStream(FileInputStream(signedCpbFile.toString())).use {
+            val jarEntries = mutableListOf<ZipEntry>()
+            var jarEntry: JarEntry? = it.nextJarEntry
+            while (jarEntry != null) {
+                jarEntries.add(jarEntry)
+                jarEntry = it.nextJarEntry
+            }
+
+            assertThat(jarEntries.map { it.name }).containsAll(listOf("META-INF/CPB-SIG.SF", "META-INF/CPB-SIG.RSA"))
+        }
+    }
+
+    private fun getPrivateKeyAndCertPath(): Pair<PrivateKey, CertPath> {
+        val passwordCharArray = "keystore password".toCharArray()
+        val privateKeyEntry = KeyStore.getInstance(testKeyStore.toFile(), passwordCharArray).getEntry(
+            "signing key 2",
+            KeyStore.PasswordProtection(passwordCharArray)
+        ) as? KeyStore.PrivateKeyEntry ?: error("Alias \"${"signing key 2"}\" is not a private key")
+
+        val privateKey = privateKeyEntry.privateKey
+        val certPath = CertificateFactory
+            .getInstance("X.509")
+            .generateCertPath(privateKeyEntry.certificateChain.asList())
+        return privateKey to certPath
     }
 
 //    @Test
