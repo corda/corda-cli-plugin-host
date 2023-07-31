61f53cd899f3d87e2818f50135bb06ffa2e13018
eunjee.yang <eunjee.yang@r3.com>
2023-07-31 14:40:52 +0100

    Revert "CORE-15785: change system lambda library to alternative"
    This reverts commit aba3cbc8ac2e2a5a25b849183c807371a6ab58ee.

--- app/src/test/kotlin/net/corda/cli/application/commands/SetCurrentNodeCommandTest.kt
+++ app/src/test/kotlin/net/corda/cli/application/commands/SetCurrentNodeCommandTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.yaml.snakeyaml.Yaml
 import picocli.CommandLine
-import uk.org.webcompere.systemstubs.environment.EnvironmentVariables
 import java.io.FileInputStream
 import java.io.FileWriter
 import java.util.*
@@ -18,24 +17,21 @@ class SetCurrentNodeCommandTest {
     fun setCurrentNodeUrlTest() {
 
         val randomPrefix = UUID.randomUUID()
-       EnvironmentVariables(
-            "CORDA_CLI_HOME_DIR",
-            "build/test-data/.cli-host/${randomPrefix}/"
-        ).execute {
-            Files.cliHomeDir().mkdirs()
 
-            val testUrl = "www.${UUID.randomUUID()}.com"
-            var data: MutableMap<String, Any> = mutableMapOf(Pair("url", "emptyUrl"))
-            val yaml = Yaml()
+        System.setProperty("CORDA_CLI_HOME_DIR", "build/test-data/.cli-host/${randomPrefix}/")
+        Files.cliHomeDir().mkdirs()
 
-            yaml.dump(data, FileWriter(Files.profile))
+        val testUrl = "www.${UUID.randomUUID()}.com"
+        var data: MutableMap<String, Any> = mutableMapOf(Pair("url", "emptyUrl"))
+        val yaml = Yaml()
 
-            CommandLine(
-                App()
-            ).execute("set-node", "-t=$testUrl")
+        yaml.dump(data, FileWriter(Files.profile))
 
-            data = yaml.load(FileInputStream(Files.profile))
-            assertEquals(testUrl, data["url"])
-        }
+        CommandLine(
+            App()
+        ).execute("set-node", "-t=$testUrl")
+
+        data = yaml.load(FileInputStream(Files.profile))
+        assertEquals(testUrl, data["url"])
     }
 }
