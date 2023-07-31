aba3cbc8ac2e2a5a25b849183c807371a6ab58ee
eunjee.yang <eunjee.yang@r3.com>
2023-07-31 09:23:06 +0100

    CORE-15785: change system lambda library to alternative

--- app/src/test/kotlin/net/corda/cli/application/commands/SetCurrentNodeCommandTest.kt
+++ app/src/test/kotlin/net/corda/cli/application/commands/SetCurrentNodeCommandTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.yaml.snakeyaml.Yaml
 import picocli.CommandLine
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables
 import java.io.FileInputStream
 import java.io.FileWriter
 import java.util.*
@@ -17,21 +18,24 @@ class SetCurrentNodeCommandTest {
     fun setCurrentNodeUrlTest() {
 
         val randomPrefix = UUID.randomUUID()
+       EnvironmentVariables(
+            "CORDA_CLI_HOME_DIR",
+            "build/test-data/.cli-host/${randomPrefix}/"
+        ).execute {
+            Files.cliHomeDir().mkdirs()
 
-        System.setProperty("CORDA_CLI_HOME_DIR", "build/test-data/.cli-host/${randomPrefix}/")
-        Files.cliHomeDir().mkdirs()
+            val testUrl = "www.${UUID.randomUUID()}.com"
+            var data: MutableMap<String, Any> = mutableMapOf(Pair("url", "emptyUrl"))
+            val yaml = Yaml()
 
-        val testUrl = "www.${UUID.randomUUID()}.com"
-        var data: MutableMap<String, Any> = mutableMapOf(Pair("url", "emptyUrl"))
-        val yaml = Yaml()
+            yaml.dump(data, FileWriter(Files.profile))
 
-        yaml.dump(data, FileWriter(Files.profile))
+            CommandLine(
+                App()
+            ).execute("set-node", "-t=$testUrl")
 
-        CommandLine(
-            App()
-        ).execute("set-node", "-t=$testUrl")
-
-        data = yaml.load(FileInputStream(Files.profile))
-        assertEquals(testUrl, data["url"])
+            data = yaml.load(FileInputStream(Files.profile))
+            assertEquals(testUrl, data["url"])
+        }
     }
 }
--- app/src/test/kotlin/net/corda/cli/application/utils/FilesTest.kt
+++ app/src/test/kotlin/net/corda/cli/application/utils/FilesTest.kt
@@ -1,5 +1,6 @@
 package net.corda.cli.application.utils
 
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.nio.file.Paths
@@ -8,21 +9,25 @@ class FilesTest {
 
     @Test
     fun testCliHomeDirNoEnvVar() {
-        System.setProperty("CORDA_CLI_HOME_DIR", "")
-        assertEquals(
-            Paths.get(System.getProperty("user.home"), "/.corda/cli/").toFile().absolutePath,
-            Files.cliHomeDir().absolutePath
-        )
+
+        EnvironmentVariables("CORDA_CLI_HOME_DIR", "").execute {
+            assertEquals(
+                Paths.get(System.getProperty("user.home"), "/.corda/cli/").toFile().absolutePath,
+                Files.cliHomeDir().absolutePath
+            )
+        }
     }
 
     @Test
     fun testCliHomeDirWithEnvVar() {
 
         val workingDir = Paths.get("").toAbsolutePath().toString()
-        System.setProperty("CORDA_CLI_HOME_DIR", "build/test-data/.cli-host/")
-        assertEquals(
-            Paths.get(workingDir,"build/test-data/.cli-host/").toString(),
-            Files.cliHomeDir().absolutePath
-        )
+
+        EnvironmentVariables("CORDA_CLI_HOME_DIR", "build/test-data/.cli-host/").execute {
+            assertEquals(
+                Paths.get(workingDir,"build/test-data/.cli-host/").toString(),
+                Files.cliHomeDir().absolutePath
+            )
+        }
     }
 }
